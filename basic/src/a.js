367aa973871e2a0c312967cb10331ca54672ec50
liangchaofei <chaofei_liang@126.com>
2019-09-19 09:28:44 +0800

    add

---
+++ basic/src/a.js
@@ -0,0 +1,8 @@
+/*
+ * @Description: In User Settings Edit
+ * @Author: your name
+ * @Date: 2019-09-18 21:04:45
+ * @LastEditTime: 2019-09-18 21:04:45
+ * @LastEditors: your name
+ */
+var str = 'a';
---
+++ basic/src/a.ts
@@ -0,0 +1,8 @@
+/*
+ * @Description: In User Settings Edit
+ * @Author: your name
+ * @Date: 2019-09-18 21:04:45
+ * @LastEditTime: 2019-09-18 21:52:43
+ * @LastEditors: Please set LastEditors
+ */
+let str21: string = 'a'
